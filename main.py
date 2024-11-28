18d27feea302d1a3fb753cab08e957d147eec8d5
Darshan KC <darshanbahadurchhetri@gmail.com>
2024-11-28 22:16:45 +0545

    first

---
+++ main.py
@@ -0,0 +1,9 @@
+class Vigenere:
+    def __init__(self, shift) -> None:
+        self.shift = shift
+        
+    def encrypt(self, secret, message):
+        pass
+    
+    def decrypt(self, secret, message);
+        pass
