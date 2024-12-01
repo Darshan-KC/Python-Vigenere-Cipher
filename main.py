34ca5d02bab816264926a83d31e5d593add94318
Darshan KC <darshanbahadurchhetri@gmail.com>
2024-12-01 21:57:08 +0545

    vigenere cipher

--- main.py
+++ main.py
@@ -16,7 +16,17 @@ class Vigenere:
         return result
     
     def decrypt(self, message) ->str:
-        pass
+        keyword = (self.keyword * (len(message) // len(self.keyword) + 1))[:len(message)]
+        result = ''
+        for i, char in enumerate(message):
+            shift = ord(keyword[i].lower()) - ord('a')
+            if 'A' <= char <= 'Z':
+                result += chr((ord(char) - ord('A') - shift) % 26 + ord('A'))
+            elif 'a' <= char <='z':
+                result += chr((ord(char) - ord('a') - shift) % 26 + ord('a'))
+            else:
+                result += char
+        return result
     
 def main():
     # try:
